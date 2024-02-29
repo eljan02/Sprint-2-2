b4b13375601ad0cfa3836cd968de9bc956232e30
eljan02 <153935255+eljan02@users.noreply.github.com>
2024-02-29 16:47:23 +0600

    Add files via upload

---
+++ src/servlets/HomeServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(value = "/home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        for (int i = 0; i < 10; i++) {
+            out.print("<h1>HELLO BITLAB</h1>");
+        }
+    }
+}
